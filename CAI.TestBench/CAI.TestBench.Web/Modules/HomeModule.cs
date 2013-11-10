6eb33494beecd9ed58f6eaf2967bf8a18985fc2d
unknown <josh.oakes@A00037.sc.tsw.com.au>
2013-11-11 09:43:55 +1000

    Renamed solution directory and .sln

---
+++ CAI.TestBench/CAI.TestBench.Web/Modules/HomeModule.cs
@@ -0,0 +1,79 @@
+﻿namespace CAI.TestBench.Web.Modules
+{
+    using System;
+    using System.Linq;
+    using Business;
+    using Model;
+    using Nancy;
+    using Nancy.ModelBinding;
+    using Raven.Client;
+
+    public class HomeModule : NancyModule
+    {
+        private readonly ICaiSettingsRepository _caiSettingsRepository;
+        private const string SettingsItemName = "Settings";
+
+        private readonly CaiServiceSettings _defaultCaiServiceSettings = new CaiServiceSettings()
+        {
+            BranchNumber = 992,
+            ServiceId = 602,
+            Username = "NET",
+            Organisation = "TEST_PNCS",
+            LastUpdated = DateTime.Now
+        };
+
+        public HomeModule(ICaiSettingsRepository caiSettingsRepository)
+        {
+            _caiSettingsRepository = caiSettingsRepository;
+
+            Get["/"] = p => View["home"];
+            Get["/about"] = p => View["about"];
+            Get["/cai-status"] = p => View["cai-status"];
+            Get["/settings/{restore?}"] = GetSettings;
+            Post["/settings"] = UpdateSettings;
+        }
+
+        private dynamic GetSettings(dynamic @params)
+        {
+            if (Request.Query["restore"])
+            {
+                // retrieve from data store
+                return View["settings", _defaultCaiServiceSettings];
+            }
+
+            if (Session[SettingsItemName] != null)
+            {
+                var settings = Session[SettingsItemName] as CaiServiceSettings;
+                Session.Delete(SettingsItemName);
+                return View["settings", settings];
+            }
+
+            // TODO retrieve from data store
+            return View["settings", _defaultCaiServiceSettings];
+        }
+
+        private dynamic UpdateSettings(dynamic @params)
+        {
+            try
+            {
+                var settings = this.BindAndValidate<CaiServiceSettings>();
+                settings.LastUpdated = DateTime.Now;
+                if (ModelValidationResult.IsValid)
+                {
+                    // TODO persist to data store
+                    Session[SettingsItemName] = settings;
+                    return Response.AsRedirect("/settings");   
+                }
+
+                return View["settings", settings];
+            }
+            catch (ModelBindingException ex)
+            {
+                var invalidProperties = ex.PropertyBindingExceptions.Select(x => x.PropertyName).ToArray();
+                var settings = this.BindAndValidate<CaiServiceSettings>(invalidProperties);
+                settings.LastUpdated = DateTime.Now;
+                return View["/settings", settings];
+            }
+        }
+    }
+}
---
+++ CAI.TestBench/CAI.TestBench.Web/TestBenchBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿namespace CAI.TestBench.Web
+{
+    using Nancy;
+    using Nancy.TinyIoc;
+    using Raven.Client;
+    using Raven.Client.Embedded;
+
+    public class TestBenchBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Nancy.Session.CookieBasedSessions.Enable(pipelines);
+            RegisterRavenDb(container);
+        }
+
+        private void RegisterRavenDb(TinyIoCContainer container)
+        {
+            IDocumentStore documentStore = new EmbeddableDocumentStore() { ConnectionStringName = "RavenDB" };
+            documentStore.Initialize();
+            container.Register<IDocumentStore>(documentStore);
+        }
+    }
+}
